5f0db03c608ba1d3533180d310e4ff16f0b377f0
csergey8 <c.sergey@protonmail.com>
2018-01-11 12:10:43 +0200

    added toolbar component for desktop

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import NavigationItem from './NavigationItem/NavigationItem';
+
+import classes from './NavigationItems.css';
+
+
+const navgationItems = () => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem
+    link="/" active >
+    Burger Builder
+    </NavigationItem>
+    <NavigationItem
+    link="/">
+    Checkout
+    </NavigationItem>
+  </ul>
+);
+
+
+export default navgationItems;
