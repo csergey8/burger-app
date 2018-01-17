92d7a7b1beaced8f070ba5d96277c93c125b8d01
csergey8 <c.sergey@protonmail.com>
2018-01-17 11:31:48 +0200

    added validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -119,6 +119,24 @@ class ContactData extends Component {
       });
   };
 
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifire) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -126,6 +144,7 @@ class ContactData extends Component {
 
     const updatedFormElement = { ...updatedOrderForm[inputIdentifire] };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputIdentifire] = updatedFormElement;
     this.setState({ orderForm: updatedOrderForm });
   };
