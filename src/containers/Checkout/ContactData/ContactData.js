7b2f65bf7dadc9b335c83a1e7d2d24bcf2263979
csergey8 <c.sergey@protonmail.com>
2018-01-21 16:22:24 +0200

    added redirect in checkout

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -94,8 +94,7 @@ class ContactData extends Component {
         valid: true
       }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   };
 
   orderHandler = event => {
@@ -182,7 +181,7 @@ class ContactData extends Component {
         </Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
     return (
@@ -197,12 +196,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading  
   };
 };
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData));
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,20 +7,26 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_START:
+      return {
+        ...state,
+        loading: true
+      };
     case actionTypes.PURCHASE_BURGER_SUCCESS:
       const newOrder = {
         ...action.orderData,
         id: action.orderId
-      }
+      };
       return {
         ...state,
         loading: false,
         orders: state.orders.concat(newOrder)
-      }
+      };
     case action.PURCHASE_BURGER_FAIL:
       return {
-
-      }
+        ...state,
+        loading: false
+      };
     default:
       return state;
   }
