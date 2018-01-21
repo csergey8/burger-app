72823979cc9aac7f2c333921d44737e94dca5b7d
csergey8 <c.sergey@protonmail.com>
2018-01-21 10:19:21 +0200

    added action creators

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
-import reducer from './store/reducer';
-
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware()));
+const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
@@ -20,7 +20,6 @@ const app = (
     </BrowserRouter>
   </Provider>
 );
-
 
 ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
